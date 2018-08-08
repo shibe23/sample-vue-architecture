c4154330ed3b8677267bd959253d0368caa3b74f
shibe23 <shibe23.info@gmail.com>
2018-08-08 09:30:54 +0900

    add module

---
+++ src/store/modules/cart.js
@@ -0,0 +1,14 @@
+export default {
+  state: {
+    test: "test"
+  },
+  getters: {
+
+  },
+  actions: {
+
+  },
+  mutations: {
+
+  },
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cart msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Cart from '@/components/Cart.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Cart
   }
 }
 </script>
